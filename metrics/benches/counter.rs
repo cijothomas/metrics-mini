5d6d593459ee394b07a2855c5dca6f748d96f175
Cijo Thomas <cijo.thomas@gmail.com>
2024-07-01 17:51:54 -0700

    fix benchmark

--- metrics/benches/counter.rs
+++ metrics/benches/counter.rs
@@ -1,20 +1,20 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use metrics::counter::Counter;
+use metrics::{common::KeyValue, counter::Counter};
 
 // cargo bench --bench counter
 pub fn counter_benchmark(c: &mut Criterion) {
     let tracker = Counter::new_with_periodic_flush();
 
-    let attributes1 = [("key1", "value1")];
+    let attributes1 = [KeyValue::new("key1", "value1")];
 
-    let attributes3 = [("key1", "value1"), ("key2", "value2"), ("key3", "value3")];
+    let attributes3 = [KeyValue::new("key1", "value1"), KeyValue::new("key2", "value2"), KeyValue::new("key3", "value3")];
 
     let attributes5 = [
-        ("key1", "value1"),
-        ("key2", "value2"),
-        ("key3", "value3"),
-        ("key4", "value4"),
-        ("key5", "value5"),
+        KeyValue::new("key1", "value1"),
+        KeyValue::new("key2", "value2"),
+        KeyValue::new("key3", "value3"),
+        KeyValue::new("key4", "value4"),
+        KeyValue::new("key5", "value5"),
     ];
 
     c.bench_function("counter_0", |b| {
